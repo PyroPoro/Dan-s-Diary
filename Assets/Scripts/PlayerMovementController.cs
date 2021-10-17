95a4cc722ca892de1b74d6484afd4741b682523b
PyroPoro <rickywen12345@gmail.com>
2021-10-17 18:38:20 -0400

    Initial Commit

---
+++ Assets/Scripts/PlayerMovementController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementController : MonoBehaviour
+{
+    public float moveSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.W)){
+            transform.Translate(0, moveSpeed, 0);
+        }
+        if(Input.GetKey(KeyCode.S)){
+            transform.Translate(0, -moveSpeed, 0);
+        }
+        if(Input.GetKey(KeyCode.A)){
+            transform.Translate(-moveSpeed, 0, 0);
+        }
+        if(Input.GetKey(KeyCode.D)){
+            transform.Translate(moveSpeed, 0, 0);
+        }
+    }
+}
