dfbe95bf6cbb2563c824f14a5a82a0a23b324185
PyroPoro <rickywen12345@gmail.com>
2021-10-27 22:35:09 -0400

    dan idle animation added and updated movement script to include direction changing

--- Assets/Scripts/PlayerMovementController.cs
+++ Assets/Scripts/PlayerMovementController.cs
@@ -7,10 +7,18 @@ public class PlayerMovementController : MonoBehaviour
     public float moveSpeed;
     public Rigidbody2D rb;
     private Vector2 moveDirection;
+    private bool isFacingLeft = true;
+    public GameObject player;
+    public float characterScale = 0.13f;
 
     void Update(){
         ProcessInputs();
-
+        if (isFacingLeft){
+            player.transform.localScale = new Vector2(1, 1) * characterScale;
+        }
+        else {
+            player.transform.localScale = new Vector2(-1, 1) * characterScale;
+        }
     }
     void FixedUpdate()
     {
@@ -25,5 +33,13 @@ public class PlayerMovementController : MonoBehaviour
 
     void Move(){
         rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
+        if (rb.velocity.x > 0)
+        {
+            isFacingLeft = false;
+        }
+        else if (rb.velocity.x < 0){
+            isFacingLeft = true;
+        }
+        Debug.Log("switch");
     }
 }
